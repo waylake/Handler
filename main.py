e54a3242724d1d2dc33b95639a76e660212c3cff
Davekii <84781282+Davekii@users.noreply.github.com>
2022-04-17 03:06:45 +0900

    edit main

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from handlers import ErrHandler
-from handlers import DBHandler
+#  from handlers import ErrHandler
+#  from handlers import DBHandler
 
+from handlers.DBHandler import DBHandler
+from handlers.ErrHandler import ErrorHandler
+
+
+DB = DBHandler.selectDB(DB_NAME = 'RAW')
