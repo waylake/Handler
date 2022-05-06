fca7859c7a49e620e81a2620e486e94b582b4671
waylake <hwangdoyeonyu@gmail.com>
2022-05-06 15:29:31 +0900

    add dabase example file

---
+++ refactoring/model/DBHandler/utils/info/database_ex.py
@@ -0,0 +1,29 @@
+db_info = {
+    'db_name':{
+        'host': 'host',
+        'user': 'user',
+        'passwd': 'passwd',
+        'port': 'port',
+    },
+}
+
+table_list = [
+    'table_name',
+    'table_name',
+    'table_name',
+]
+
+table_info ={
+    'table_name': {
+        'scheme':[
+            'scheme_list',
+            'scheme_list',
+            'scheme_list',
+        ],
+        'to_scheme':{
+            'scheme_name': 'scheme_name',
+            'scheme_name': 'scheme_name',
+            'scheme_name': 'scheme_name',
+        }
+    },
+}
