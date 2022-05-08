e02b4f50789dcec0c361015898e46694aef3d979
waylake <hwangdoyeonyu@gmail.com>
2022-05-09 07:38:08 +0900

    add DBHandler's utils that max_length_check

--- refactoring/model/DBHandler/utils/utils.py
+++ refactoring/model/DBHandler/utils/utils.py
@@ -1,5 +1,6 @@
 from .info import _database
 import pymysql
+from tqdm import tqdm
 
 
 class Utils:
@@ -22,14 +23,14 @@ class Utils:
     def insert_db(self, input_rows, input_conn, table_info):
         _conn = input_conn
         _curs = _conn.cursor()
-        table_name = table_info['table_info']
-        table_scheme = table_info['scheme']
+        table_name = self._info.table_info['table_info']
+        table_scheme = self._info.table_info['scheme']
         scheme_format = tuple(table_scheme)
         values_format = ['%s' for i in range(len(table_scheme))]
         values_format = tuple(values_format)
 
         try:
-            for row in input_rows:
+            for row in tqdm(input_rows):
                 sql = "INSERT INTO RAW.{}".format(table_name) + \
                     "({})".format(",\n ".join(scheme_format)) + \
                     " VALUES({})".format(",\n".join(values_format))
@@ -41,3 +42,19 @@ class Utils:
             print(e)
             _conn.commit()
             _conn.close()
+
+    def max_length_check(self, rows):
+        """
+        get each element's max length from rows
+        """
+        elements = [str(1) for i in range(len(rows[0]))]
+        row_lens = [None for i in range(len(elements))]
+
+        for rows_idx, row in enumerate(rows):
+            for row_idx, row_element in enumerate(row):
+                row_element_len = len(str(row_element))
+                element_row_len = len(elements[row_idx])
+                if row_element_len > element_row_len:
+                    row_lens[row_idx] = row_element_len
+
+        return row_lens
